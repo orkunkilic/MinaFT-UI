b9c0aed2efc249a1de8fd803136e780e6ee7e254
orkunkilic <orkun.kilic@boun.edu.tr>
2022-04-24 05:41:10 +0300

    new pic

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,7 +2,7 @@ import { Card, Image } from 'antd'
 import Meta from 'antd/lib/card/Meta'
 import Text from 'antd/lib/typography/Text'
 import React from 'react'
-import Bora from '../assets/bora.jpeg'
+import Bora from '../assets/nft.webp'
 import SendOutlined from '@ant-design/icons/SendOutlined'
 export default function Item({item, setModal}) {
   return (
